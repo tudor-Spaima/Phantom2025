de2850a38749b5cc87c26f4f58ffc528109e3c74
Tudor <53434734+tudor-Spaima@users.noreply.github.com>
2025-01-24 19:09:42 +0200

    Update Teleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Teleop.java
@@ -372,7 +372,6 @@ public class Teleop extends LinearOpMode {
 
             String detectedColor = senzori.detectColor(senzori.senzorIntakeCuloare.red(), senzori.senzorIntakeCuloare.green(),senzori.senzorIntakeCuloare.blue());
             telemetry.addData("Automatizare Intake", automatizareInakte);
-            telemetry.speak( "nigger" );
             telemetry.addData("Are sample in gura ", senzori.hasSample());
             telemetry.addData("Current State", currentState);
             telemetry.addData("Detected Color", detectedColor);
